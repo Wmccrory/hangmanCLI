3a533afa9c53a49274a4958ba191213a032493e5
Will <wmccrory@gmail.com>
2018-02-19 18:18:53 -0600

    Uploading current build

--- letter.js
+++ letter.js
@@ -7,45 +7,24 @@ var Letter = function() {
 		name: input.toUpperCase(),
 		guessed: false,
 		};
-
-		Letter.prototype.toString = function(input) {
-			console.log("TestTEst");
-			console.log(newLetter.name);
-			var testMe = newLetter.name;
-			if (input.guessed) {
-				console.log(testMe);
-			} else {
-				console.log("_");
-			}
-		}
-
-		console.log(newLetter);
 	}
 
 	//Display letter or _ depending on if user has guessed correct letter
-	// this.toString = function(input) {
-	// 	if (input.guessed) {
-	// 		console.log(input.name);
-	// 	} else {
-	// 		console.log("_");
-	// 	}
-	// }
+	this.toString = function(input) {
+		if (input.guessed) {
+			return input.name;
+		} else {
+			return "_";
+		}
+	}
 
 	//Check to see if user input matches any letters
 	this.letterChecker = function(input, variable) {
 		if (input.toUpperCase() === variable.name) {
 			variable.guessed = true;
-			console.log(variable);
-		} else {
-			console.log(variable);
 		}
 	}
 }
 
-// var letter = new Letter();
-// letter.letterInput("a");
-// letter.toString(newLetter);
-// letter.letterChecker("a", newLetter);
-// letter.toString(newLetter);
-
 module.exports = Letter;
+
